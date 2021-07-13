61df8ab6ec66bb8b9bbf7d8ee3935981a69507c6
Angel Olmedo Garcia <angelixus99@gmail.com>
2021-07-14 00:10:52 +0200

    Final push

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
 const multer = require('multer');
 const path = require('path');
 const uuid = require('uuid');
 const fs = require('fs');
 const { exec } = require("child_process");
 var sizeOf = require('image-size');
-const { dir } = require('console');
 
 const dirBase = './images';
 const dirPositive = './images/goodConditions'
@@ -31,8 +29,6 @@ app.listen(8080, () => {
     console.log('Listening on port 8080')
 })
 
-app.use(morgan('short'))
-
 
 const storageGoodCondition = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -51,7 +47,7 @@ app.post('/goodConditionImages', uploadGoodCondition.array('files'), (req, res) 
     let filesLengthGood = 0;
     fs.readdir('./images/goodConditions', (err, files) => {
         if(err) {
-            return res.send(`error: ${err.message}`);
+            return res.status(500).json(`error: ${err.message}`);
         } else {
             let stringToWrite = ''
             filesLengthGood = files.length
@@ -63,29 +59,26 @@ app.post('/goodConditionImages', uploadGoodCondition.array('files'), (req, res) 
             stringToWrite = stringToWrite.slice(0, -1)
             fs.writeFile(goodConditionTxtName, stringToWrite, function (err) {
                 if (err) {
-                    return res.send(`error: ${err.message}`);
+                    return res.status(500).json(`error: ${err.message}`);
                 } 
               });
         }
 
         const widthWindow = 24
-        const heightWindow = 24
+        const heightWindow = 12
         const sample = 0.9
 
-        exec(`opencv_createsamples -info ${goodConditionTxtName} -vec pos.vec -w ${widthWindow} -h ${heightWindow} -num ${filesLengthGood * 4}`, (error, stdout, stderr) => {
+        exec(`opencv_createsamples -info ${goodConditionTxtName} -vec pos.vec -w ${widthWindow * 5} -h ${heightWindow * 5} -num ${filesLengthGood * 4}`, (error, stdout, stderr) => {
             if (error) {
-                return res.send(`error: ${error.message}`);
-            }
-            if (stderr) {
-                //return res.send(`stderr: ${stderr}`);
+                return res.status(500).json(`error: ${error.message}`);
             }
 
-            exec(`opencv_traincascade -data cascade/ -vec pos.vec -bg bg.txt -w ${widthWindow} -h ${heightWindow} -numPos ${Math.floor(filesLengthGood * sample)} -numNeg ${Math.floor((filesLengthGood * sample) / 2)} -numStages 10 -featureType HAAR -stageType BOOST`, (error, stdout, stderr) => {
+            exec(`opencv_traincascade -data cascade/ -vec pos.vec -bg bg.txt -w ${widthWindow * 5} -h ${heightWindow * 5} -numPos ${Math.floor(filesLengthGood * sample)} -numNeg ${Math.floor((filesLengthGood * sample) / 2)} -numStages 10 -featureType HAAR -stageType BOOST`, (error, stdout, stderr) => {
                 if (error) {
-                    return res.send(`error: ${error.message}`);
+                    return res.status(500).json(`error: ${error.message}`);
                 }
                 if (stderr) {
-                    return res.send(`stderr: ${stderr}`);
+                    return res.status(500).json(`error: ${stderr}`);
                 }
 
                 let filePath = path.join(__dirname, 'cascade/cascade.xml') 
@@ -99,12 +92,12 @@ app.post('/goodConditionImages', uploadGoodCondition.array('files'), (req, res) 
 
                 fs.readdir('./images/goodConditions', (err, files) => {
                     if (err) {
-                        return res.send(`error: ${err.message}`);
+                        return res.status(500).json(`error: ${err.message}`);
                     }
                     files.forEach((file) => {
                         fs.unlink(path.join('./images/goodConditions', file), err => {
                             if (err) {
-                                return res.send(`error: ${err.message}`);
+                                return res.status(500).json(`error: ${err.message}`);
                             }
                         })
                     })
@@ -112,25 +105,12 @@ app.post('/goodConditionImages', uploadGoodCondition.array('files'), (req, res) 
 
                   fs.readdir('./cascade', (err, files) => {
                     if (err) {
-                        return res.send(`error: ${err.message}`);
+                        return res.status(500).json(`error: ${err.message}`);
                     }
                     files.forEach((file) => {
                         fs.unlink(path.join('./cascade', file), err => {
                             if (err) {
-                                return res.send(`error: ${err.message}`);
-                            }
-                        })
-                    })
-                  })
-
-                  fs.readdir('./cascade', (err, files) => {
-                    if (err) {
-                        return res.send(`error: ${err.message}`);
-                    }
-                    files.forEach((file) => {
-                        fs.unlink(path.join('./cascade', file), err => {
-                            if (err) {
-                                return res.send(`error: ${err.message}`);
+                                return res.status(500).json(`error: ${err.message}`);
                             }
                         })
                     })
@@ -138,33 +118,17 @@ app.post('/goodConditionImages', uploadGoodCondition.array('files'), (req, res) 
 
                   fs.unlink('pos.txt', err => {
                         if (err) {
-                            return res.send(`error: ${err.message}`);
+                            return res.status(500).json(`error: ${err.message}`);
                         }
                     })
                 
                     fs.unlink('pos.vec', err => {
                         if (err) {
-                            return res.send(`error: ${err.message}`);
+                            return res.status(500).json(`error: ${err.message}`);
                         }
                     })
                   return;
             })
         });
     })
-})
-
-app.get('/imagesGoodList', (req, res) => {
-    fs.readdir('./images/goodConditions', (err, files) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`numfiles: ${files.length}`);     
-     });
-})
-
-app.get('/imagesBadList', (req, res) => {
-    fs.readdir('./images/badConditions', (err, files) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`numfiles: ${files.length}`);     
-     });
 })
